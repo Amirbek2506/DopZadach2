abd87c2a6952a304bb8785e713e635726b1606ec
Amirbek2506 <62469848+Amirbek2506@users.noreply.github.com>
2020-03-25 14:49:17 +0300

    created console .net core project

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MyprojecsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*
+                                     //Задача 1:
+            int A,B;
+         Console.Write("A = ");
+         A=Convert.ToInt32(Console.ReadLine());
+         Console.Write("B = ");
+         B=Convert.ToInt32(Console.ReadLine());
+         if(A!=B)
+         {
+             if(A>B) B=A; else A=B;
+         }
+         else if(A==B) A=B=0;
+         Console.WriteLine($"A = {A}  B = {B}");
+
+                                     //Задача 2:
+         double Operand1,Operand2;
+         Console.Write("Operand1 = ");
+         Operand1=Convert.ToDouble(Console.ReadLine());
+         Console.Write("Operand2 = ");
+         Operand2=Convert.ToDouble(Console.ReadLine());
+         Console.Write("Введите знак арифметической операции( -  +  *  / ): ");
+         string sign=Console.ReadLine();
+         switch(sign)
+         {
+             case "+": Console.WriteLine($"Operand1 + Operand2 = {Operand1+Operand2}");break;
+             case "-": Console.WriteLine($"Operand1 - Operand2 = {Operand1-Operand2}");break;
+             case "*": Console.WriteLine($"Operand1 * Operand2 = {Operand1*Operand2}");break;
+             case "/":
+             {
+                 if(Operand2==0) Console.WriteLine("Error 'division with zero!'");
+                 else Console.WriteLine($"Operand1 / Operand2 = {Operand1/Operand2}");
+                 break;
+             } 
+         }
+                                     //Задача 3:
+
+            Console.Write(" Введите число: ");
+         double Chislo=Convert.ToDouble(Console.ReadLine());
+            if((Chislo<0)||(Chislo>100)) Console.WriteLine(" Вне диапазона! ");
+            else
+            {
+                if(Chislo<=14) Console.WriteLine("Число находится в диапазона [0;14]");
+                if((Chislo>14)&&(Chislo<=35)) Console.WriteLine("Число находится в диапазона [15;35]");
+                if((Chislo>35)&&(Chislo<=50)) Console.WriteLine("Число находится в диапазона [36;50]");
+                if(Chislo>50) Console.WriteLine("Число находится в диапазона [50;100]");
+            }
+            */
+        }
+    }
+}
